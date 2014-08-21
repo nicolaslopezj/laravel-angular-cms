279097c441b6d2b858830b967c9ca29ce4ccb245
Nicolás López <nicolaslopezj@me.com>
2014-08-20 22:20:04 -0400

    better login/remind/reset layout

--- app/views/auth/layout.blade.php
+++ app/views/auth/layout.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.master')
 
 @section('structure')
-{{ $content }}
+<br><br>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
+			{{ $content }}
+		</div>
+	</div>
+</div>
 @stop
--- app/views/auth/login.blade.php
+++ app/views/auth/login.blade.php
@@ -1,35 +1,37 @@
-<br><br>
 {{ Form::open(['route' => 'auth.login.post']) }}
-<div class="row">
-	<div class="col-sm-4 col-sm-offset-4">
-		<h3>Login</h3>
-		<hr><br>
-		@if (Session::get('url.intended'))
-		<div class="alert alert-warning">You need to log in to continue.</div>
-		@endif
-		@if (Session::get('error'))
-		<div class="alert alert-danger">{{ Session::get('error') }}</div>
-		@else
-		<br>
-		@endif
+<h3>Login</h3>
+<hr><br>
+@if (Session::get('url.intended'))
+<div class="alert alert-warning">You need to log in to continue.</div>
+@endif
+@if (Session::get('error'))
+<div class="alert alert-danger">{{ Session::get('error') }}</div>
+@endif
 
-		<div class="form-group">
-			<label>Email</label>
-			<input type="email" class="form-control" name="email">
-		</div>
-		<div class="form-group">
-			<a class="pull-right" href="{{ URL::action('Cms\Auth\Controllers\RemindersController@getRemind') }}">Forgot your password?</a>
-			<label>Password</label>
-			<input type="password" class="form-control" name="password">
-		</div>
-		<div class="form-group text-right">
-			<label class="checkbox">
-				<input type="checkbox" name="remember">
-				Remember Me
-			</label>
-		</div>
-
-		<button class="btn btn-primary pull-right">Login</button>
-	</div>
+<div class="form-group">
+	<label>Email</label>
+	<input type="email" class="form-control" name="email">
 </div>
+<div class="form-group">
+	<label>Password</label>
+	<input type="password" class="form-control" name="password">
+</div>
+<div class="clearfix">
+	<div class="form-group pull-right">
+		<label class="checkbox">
+			<input type="checkbox" name="remember">
+			Remember Me
+		</label>
+	</div>
+	<a href="{{ URL::action('Cms\Auth\Controllers\RemindersController@getRemind') }}">
+		Forgot your password?
+	</a>
+</div>
+<style type="text/css">
+.checkbox {
+	margin: 0px;
+}
+</style>
+
+<button class="btn btn-primary pull-right">Login</button>
 {{ Form::close() }}
