863761367ab77e144b290bd954e3a16e57adfc25
Nicolás López <nicolaslopezj@me.com>
2014-09-10 19:09:21 -0300

    better directory

--- app/views/directory/views/simple-route.blade.php
+++ app/views/directory/views/simple-route.blade.php
@@ -1,18 +1,18 @@
 <!--{{ $route->name }}-->
 <div class="clearfix">
-	@if ($route->meta_title)
-		<h3>@evaluate($route->meta_title)</h3>
+	@if (array_key_exists('title', json_decode($route->directory, 1)))
+		<h3>@evaluate(json_decode($route->directory, 1)['title'])</h3>
 		<br>
 	@endif
-	@if ($route->meta_image)
+	@if (array_key_exists('image', json_decode($route->directory, 1)))
 		<img style="max-width: 100%; margin-top: 10px; margin-bottom: 10px;" src="
-		@evaluate($route->meta_image)
+		@evaluate(json_decode($route->directory, 1)['image'])
 		" alt="
-		@evaluate($route->meta_title)
+		@evaluate(json_decode($route->directory, 1)['title'])
 		">
 	@endif
-	@if ($route->meta_description)
-		<p>@evaluate($route->meta_description)</p>
+	@if (array_key_exists('description', json_decode($route->directory, 1)))
+		<p>@evaluate(json_decode($route->directory, 1)['description'])</p>
 	@endif
 	<br>
 	<a class="btn btn-primary btn-xs pull-right" href="{{ route($route->name) }}">
