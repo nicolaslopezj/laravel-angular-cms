797c09a5e1d723e10c6f33502fa03700b738bf43
Nicolás López <nicolaslopezj@me.com>
2014-10-10 22:27:31 -0300

    no se

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -9,14 +9,7 @@ class UsersTableSeeder extends Seeder {
 	 */
 	public function run()
 	{
-		$data = [
-		'name' => 'Nicolás López',
-		'email' => 'nicolaslopezj@me.com',
-		'role' => 'dev',
-		'password' => '$2y$10$xyRINiLgWZwKdcie9/bwgOfd4frPpIGVgNhQBK3oXmPsWv69Y8pOO',
-		];
-
-		User::create($data);
+		
 
 	}
 
